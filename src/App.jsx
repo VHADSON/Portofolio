b9a6ba0968309bf3978c222d2fd5a8932dd205c0
Revaldo-202 <revaldoramadana914@gmail.com>
2025-05-30 01:54:18 +0700

    penambahan section

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import DataImage from "./data";
 function App() {
   return (
     <>
-      <div className="hero grid grid-cols-2 items-center pt-10">
+      <div className="hero grid md:cols-2 items-center pt-10 xl:gap-0 gap-6 grid-cols-1">
         <div>
           <div className="flex items-center gap-3 mb-6 bg-zinc-800 w-fit p-4 rounded-2xl">
             <img
@@ -22,7 +22,7 @@ function App() {
             Packaging, Digital Art, ketertarikan pada bidang ini sudah
             berlangsung lebih dari 2 tahun untuk semua bidang.
           </p>
-          <div className="flex items-center gap-4">
+          <div className="flex items-center sm:gap-4 gap-2">
             <a
               href="#"
               className="bg-violet-700 p-4 rounded-2xl hover:bg-violet-600"
@@ -40,9 +40,51 @@ function App() {
         <img
           src={DataImage.HeroImage}
           alt="Hero Image"
-          className="w-[500px] ml-auto rounded-b-4xl"
+          className="w-[500px] md:ml-auto rounded-b-4xl"
         />
       </div>
+
+      {/* Tentang */}
+
+      <div className="tentang mt-32 py-10">
+        <div className="xl:w-2/3 lg:w-3/4 w-full mx-auto p-7 bg-zinc-800 rounded-lg">
+          <img
+            src={DataImage.HeroImage}
+            alt="Hero Image"
+            className="w-12 rounded-md mb-10 sm:hidden"
+          />
+          <p className="text-base/loose mb-10">
+            Hi, perkenalkan saya Diky Prayoga, seorang Front End Web Developer
+            dan Designer untuk UI/UX Design maupun Product Digital, Saya percaya
+            bahwa desain dan fungsionalitas harus berjalan beriringan, sehingga
+            setiap proyek yang saya kembangkan tidak hanya terlihat menarik
+            tetapi juga memberikan pengalaman pengguna yang optimal.
+          </p>
+          <div className="flex items-center justify-between">
+            <img
+              src={DataImage.HeroImage}
+              alt="Image"
+              className="w-12 rounded-md sm:block hidden"
+            />
+            <div className="flex items-center gap-6">
+              <div>
+                <h1 className="text-4xl mb-1">
+                  1<span className="text-violet-500"></span>
+                </h1>
+                <p>Proyek Selesai</p>
+              </div>
+              <div>
+                <h1 className="text-4xl mb-1">
+                  2<span className="text-violet-500">+</span>
+                </h1>
+                <p>Bulan Pengalaman</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Tentang */}
     </>
   );
 }
