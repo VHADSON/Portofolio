2b03f0ebc3a92080590156355da0eccbd27ed9c1
Revaldo-202 <revaldoramadana914@gmail.com>
2025-05-30 10:00:47 +0700

    Sedikit perubahan

--- src/App.jsx
+++ src/App.jsx
@@ -71,13 +71,13 @@ function App() {
             <div className="flex items-center gap-6">
               <div>
                 <h1 className="text-4xl mb-1">
-                  1<span className="text-violet-500"></span>
+                  1<span className="text-violet-500">+</span>
                 </h1>
                 <p>Proyek Selesai</p>
               </div>
               <div>
                 <h1 className="text-4xl mb-1">
-                  2<span className="text-violet-500">+</span>
+                  1<span className="text-violet-500">+</span>
                 </h1>
                 <p>Bulan Pengalaman</p>
               </div>
